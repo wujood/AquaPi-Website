dd9d266dbc66c3601d7a9b80bf1a62e7ee6ad20f
Sebastian Weiss <sw714201@fh-muenster.de>
2018-05-31 13:36:22 +0200

    Added functionality to view-component

--- src/app/components/view/view.component.ts
+++ src/app/components/view/view.component.ts
@@ -14,14 +14,23 @@ export class ViewComponent implements OnInit {
 
   @Input() items: ViewData[];
   @Input() title: string;
-
+  @Input() placeholder = 'Select';
   @Input() entries: ViewData[];
 
-  private selectedItem: ViewData;
+  public selectedItem: ViewData;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onChange(event) {
+    this.selectedItem = event.value;
+  }
+
+  add() {
+    this.items.push({...this.selectedItem});
+    this.selectedItem = undefined;
   }
 
 }
